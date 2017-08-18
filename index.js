b25a241b355ea6ceb7d256ad1473e7affcec956f
ahhmetatar <ahhmetatar@gmail.com>
2017-08-18 09:29:06 +0300

    Dockerized integration test initialization

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const MongoClient = require("mongodb").MongoClient;
 var db = null;
 var app = express();
 
+// Routes
 app.get("/movies", (req, res, next) => {
 
     var skipVal = Math.floor(Math.random() * 5);
@@ -23,6 +24,7 @@ app.use((err, req, res, next) => {
     next(err);
 });
 
+// Database connection
 MongoClient.connect(process.env.DB_URL).then((database) => {
     db = database;
 
