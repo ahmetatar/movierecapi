97f32e10bb6607e60bc87d31bc9f72efa68dca2f
ahhmetatar <ahhmetatar@gmail.com>
2017-08-21 15:06:51 +0300

    Test Initialization

--- dockerfile
+++ dockerfile
@@ -1,10 +1,6 @@
 FROM node
 MAINTAINER "Ahmet Atar <ahhmetatar@gmail.com>"
 
-ENV PORT=1453
-ENV NODE_ENV="production"
-ENV DB_URL="mongodb://db:27017/moviedb"
-
 COPY package.json /tmp/package.json
 RUN cd /tmp && npm install
 RUN mkdir -p /app && cp -a /tmp/node_modules /app/
--- test/movies.spec.js
+++ test/movies.spec.js
@@ -6,8 +6,17 @@ before(function (done) {
     app.initialize(done);
 });
 
-describe("Get Movies", function () {
+describe("#GET / movies", function () {
     it("should returns random five movies", function (done) {
         
+        request(app)
+            .get("/movies")
+            .expect(200)
+            .end((err, res) => {
+                expect(res.body).to.be.an("array");
+                expect(res.body.length).to.be.equal(5);
+                
+                done();
+            });
     });
 });
