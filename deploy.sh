124f8465d905a15bebde25f4623bb0dcaa3b1b73
ahmetatar <ahhmetatar@gmail.com>
2017-10-10 14:49:10 +0300

    remove default route

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ set -e
 
 gcloud container clusters create movie-cluster-1 --zone asia-east1-a
 kubectl create -f ./configs/secrets.yml
+kubectl create -f ./configs/deployment.yml
+kubectl expose deployment movierecapi-deployment --type=LoadBalancer --name=movierecapi-service
