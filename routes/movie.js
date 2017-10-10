aabcf4f388cf8be09ca3eed5353e22afa7792d02
ahmetatar <ahhmetatar@gmail.com>
2017-10-10 16:31:36 +0300

    db test code

--- routes/movie.js
+++ routes/movie.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const db = require("../infrastructure/db");
 const utils = require("../infrastructure/utils");
-
 const config = require("nconf");
 
 var router = express.Router();
 
 router.get("/", (req, res) => {
     var skipVal = utils.getRandomSkipValue();
+    
+    res.send(config.get("DB_URL"));
 
-    db.open(config.get("DB_URL")).then(() => {
-        db.collection("movies")
-            .find({}, { _id: false })
-            .limit(5)
-            .skip(skipVal)
-            .toArray((err, result) => {
-                res.json(result);
-            });
-    }).catch((err) => {
-        res.json(err);
-    });
+/*     db.collection("movies")
+        .find({}, { _id: false })
+        .limit(5)
+        .skip(skipVal)
+        .toArray((err, result) => {
+            res.json(result);
+        }); */
 });
 
 module.exports = router;
